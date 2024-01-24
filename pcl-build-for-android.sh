7be5b79c2be8463c1ae957c3921cb375cf3332bc
jono <jonatan.bergenwall@cpacsystems.se>
2024-01-24 13:20:24 +0100

    Removing example-app and fixing known bugs Removing input arg in pcl-build-for-android.sh, builds for all in loop

--- pcl-build-for-android.sh
+++ pcl-build-for-android.sh
@@ -2,50 +2,53 @@
 
 set -e
 
-PROFILE=${1}
+PROFILES=("arm64-v8a" "armeabi-v7a" "x86_64")
 
-if [ ! -f "conan-profiles/${PROFILE}" ] ; then
-  echo "${PROFILE} not supported!"
-  exit 1
-fi
+for PROFILE in "${PROFILES[@]}" ; do
+  echo "Profile ${PROFILE} is building ..."
 
-echo -e "\n\n\033[1;35m###########################################"
-echo -e "### ANDROID-TOOLCHAIN setup...          ###"
-echo -e "###########################################\033[m\n\n"
+  if [ ! -f "conan-profiles/${PROFILE}" ] ; then
+    echo "${PROFILE} not supported!"
+    exit 1
+  fi
 
-conan create -pr default conanfiles/android-toolchain bashbug/stable
+  echo -e "\n\n\033[1;35m###########################################"
+  echo -e "### ANDROID-TOOLCHAIN setup...          ###"
+  echo -e "###########################################\033[m\n\n"
 
-echo -e "\n\n\033[1;35m###########################################"
-echo -e "### FLANN cross-compiling start...      ###"
-echo -e "###########################################\033[m\n\n"
+  conan create -pr default conanfiles/android-toolchain bashbug/stable
 
-conan create -pr conan-profiles/${PROFILE} conanfiles/lz4 bashbug/stable
-conan create -pr conan-profiles/${PROFILE} conanfiles/flann bashbug/stable
+  echo -e "\n\n\033[1;35m###########################################"
+  echo -e "### FLANN cross-compiling start...      ###"
+  echo -e "###########################################\033[m\n\n"
 
-echo "FLANN cross-compiling finished!"
+  conan create -pr conan-profiles/${PROFILE} conanfiles/lz4 bashbug/stable
+  conan create -pr conan-profiles/${PROFILE} conanfiles/flann bashbug/stable
 
-echo -e "\n\n\033[1;35m###########################################"
-echo -e "### BOOST cross-compiling start...      ###"
-echo -e "###########################################\033[m\n\n"
+  echo "FLANN cross-compiling finished!"
 
-conan create -pr conan-profiles/${PROFILE} conanfiles/boost bashbug/stable
+  echo -e "\n\n\033[1;35m###########################################"
+  echo -e "### BOOST cross-compiling start...      ###"
+  echo -e "###########################################\033[m\n\n"
 
-echo "BOOST cross-compiling finished!"
+  conan create -pr conan-profiles/${PROFILE} conanfiles/boost bashbug/stable
 
-echo -e "\n\n\033[1;35mm###########################################"
-echo -e "### PCL cross-compiling start...        ###"
-echo -e "###########################################\033[m\n\n"
+  echo "BOOST cross-compiling finished!"
 
-echo -e "\n\n\033[1;32m this will run for a while... time to drink a\n"
-echo -e "   ( ( "
-echo -e "    ) ) "
-echo -e "  ........ "
-echo -e "  |      |] "
-echo -e "  \      /  "
-echo -e "   '----' \033[m\n\n"
+  echo -e "\n\n\033[1;35mm###########################################"
+  echo -e "### PCL cross-compiling start...        ###"
+  echo -e "###########################################\033[m\n\n"
 
-conan create -pr conan-profiles/${PROFILE} conanfiles/pcl bashbug/stable
+  echo -e "\n\n\033[1;32m this will run for a while... time to drink a\n"
+  echo -e "   ( ( "
+  echo -e "    ) ) "
+  echo -e "  ........ "
+  echo -e "  |      |] "
+  echo -e "  \      /  "
+  echo -e "   '----' \033[m\n\n"
 
-cp conan-profiles/${PROFILE} ~/.conan/profiles
+  conan create -pr conan-profiles/${PROFILE} conanfiles/pcl bashbug/stable
 
-echo "PCL cross-compiling finished!"
+  cp conan-profiles/${PROFILE} ~/.conan/profiles
+
+  echo "PCL cross-compiling finished!"
